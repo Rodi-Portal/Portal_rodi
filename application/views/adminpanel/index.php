6be3067e6b1bbc5e9550b1bc5daa04c368233ab0
DevRodi <rodi.control@gmail.com>
2024-07-10 16:00:17 -0600

    corrigiendo  titulos de las  graficas

--- application/views/adminpanel/index.php
+++ application/views/adminpanel/index.php
@@ -134,7 +134,7 @@
     <div class="col-md-6 d-flex align-items-stretch">
       <div class="card shadow mb-4 w-100" id="miniChartCard">
       <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-          <h6 class="m-0 font-weight-bold text-primary">Estadísticas de Reclutamiento <?php echo date('Y'); ?></h6>
+          <h6 class="m-0 font-weight-bold text-primary">Estadísticas de Reclutadores <?php echo date('Y'); ?></h6>
         </div>
         <div class="card-body">
           <div class="chart-area">
