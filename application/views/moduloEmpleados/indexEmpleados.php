888aab34803be0ec608be04c4aa3d7cc7a0a5a8e
DevRodi <rodi.control@gmail.com>
2024-10-11 11:44:05 -0600

    subiendo cambios vue

--- application/views/moduloEmpleados/indexEmpleados.php
+++ application/views/moduloEmpleados/indexEmpleados.php
@@ -2,13 +2,12 @@
  <link rel="stylesheet" href="<?php echo base_url('public/vue/css/cssEmpleados.css'); ?>">
 <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
 <div class="seccion" id="seccion1">
-    <div id="app"></div> <!-- Contenedor para el componente Vue -->
+    <div id="app" data-your-value="<?php echo $this->session->userdata('idPortal'); ?>"></div>
 </div>
 
 <script src="<?php echo base_url('public/vue/js/moduloEmpleados.js'); ?>"></script>
 <script>
 document.addEventListener('DOMContentLoaded', () => {
-    // Montar la aplicación Vue en el contenedor #app
     if (document.getElementById('app')) {
         window.mountVueApp('#app');
     }
