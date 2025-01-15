f8010d473b43511d5684904e20a293d13af536b7
DevRodi <rodi.control@gmail.com>
2025-01-15 11:55:41 -0600

    subiendo  la  segregacion de roles  y pasarela  de pago solo  para  el portal   interno para  preubas x1

--- application/views/moduloEmpleados/indexEmpleados.php
+++ application/views/moduloEmpleados/indexEmpleados.php
@@ -2,6 +2,13 @@
 
  <link rel="stylesheet" href="<?php echo base_url('public/vue/css/cssEmpleados.css'); ?>">
 <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
+<?php if($this->session->userdata('idrol') == 11 || $this->session->userdata('idrol') == 4){ ?>
+    <div class="seccion" id="seccion1">
+    <h3>No tienes   acceso  a  este  modulo</h3>
+</div>
+
+
+<?php }else{ ?>
 <div class="seccion" id="seccion1">
     <div id="app" 
          data-your-value="<?php echo $this->session->userdata('idPortal'); ?>" 
@@ -9,7 +16,7 @@
          data-your-client-value="<?php echo $cliente_id; ?>"
          data-your-rol-value="<?php echo $this->session->userdata('idrol');  ?>"></div>
 </div>
-
+<?php }?>
 <script src="<?php echo base_url('public/vue/js/moduloEmpleados.js'); ?>"></script>
 <script>
 document.addEventListener('DOMContentLoaded', () => {
