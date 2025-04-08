1bab464e5088c440bd48b4418d834651fa734d02
DevRodi <rodi.control@gmail.com>
2025-04-08 12:51:40 -0600

    subiendo   cambios de  produccion al sandbox  parte 3

--- application/controllers/Cat_UsuarioInternos.php
+++ application/controllers/Cat_UsuarioInternos.php
@@ -98,7 +98,9 @@ class Cat_UsuarioInternos extends CI_Controller
             'id_usuario' => $id_usuario,
             'id_rol' => $id_rol,
         );
-        $this->session->set_userdata('idrol', $id_rol);
+        if($idUsuario == $this->session->usersdata('id')){
+            $this->session->set_userdata('idrol', $id_rol);
+        }
         $datosGenerales = array(
             'nombre' => $nombre,
             'paterno' => $paterno,
