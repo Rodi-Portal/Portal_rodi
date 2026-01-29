5b114f2f23a585df41227e3a594161b6678bc8ba
DevRodi <rodi.control@gmail.com>
2026-01-29 15:03:57 -0600

    cambios para  el portal   definiendo rutas x1

--- application/config/email.php
+++ application/config/email.php
@@ -1,35 +1,6 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-/*
- |------------------------------------------------------------
- | CONFIGURACIÓN BASE DE EMAIL
- |------------------------------------------------------------
- */
-
-$config = array(
-    'protocol'    => 'smtp',
-    'mailtype'    => 'html',
-    'charset'     => 'utf-8',
-    'wordwrap'    => TRUE,
-    'newline'     => "\r\n",
-    'crlf'        => "\r\n"
-);
-
-/*
- |------------------------------------------------------------
- | CARGAR CONFIG PRIVADA (SI EXISTE)
- |------------------------------------------------------------
- */
-
-$private = APPPATH . 'config/email_private.php';
-
-if (file_exists($private)) {
-    require $private;
-
-    $config['smtp_host']   = $config['smtp_host'];
-    $config['smtp_user']   = $config['smtp_user'];
-    $config['smtp_pass']   = $config['smtp_pass'];
-    $config['smtp_port']   = $config['smtp_port'];
-    $config['smtp_crypto'] = $config['smtp_secure'];
-}
+$config['protocol'] = 'smtp';
+$config['mailtype'] = 'html';
+$config['charset']  = 'utf-8';
+$config['wordwrap'] = TRUE;
