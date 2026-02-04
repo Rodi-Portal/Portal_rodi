d04ce12057c92888312600f901fd4400a1d74ed9
DevRodi <rodi.control@gmail.com>
2026-02-04 12:29:02 -0600

    cambios  para  registro de candidatos  hacia  RODIx1

--- application/controllers/Client.php
+++ application/controllers/Client.php
@@ -769,13 +769,13 @@ class Client extends Custom_Controller
 
                     $ch = curl_init($url);
                     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                    curl_setopt($ch, CURLOPT_POST, true);
+                    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST'); // 👈 FUERZA POST
                     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+                    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false); // 👈 EVITA REDIRECCIONES
                     curl_setopt($ch, CURLOPT_HTTPHEADER, [
                         'Content-Type: application/json',
                         'Accept: application/json',
                     ]);
-
                     $response    = curl_exec($ch);
                     $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
                     // 👇 AGREGA ESTA LÍNEA
